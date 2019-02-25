0b1628d25774c99b83d225b08c5e468156ca6700
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-25 04:34:22 +0000

    Automatically backed up by Learn

--- db/migrate/20190219044700_create_movies.rb
+++ db/migrate/20190219044700_create_movies.rb
@@ -9,3 +9,21 @@ class CreateMovies < ActiveRecord::Migration[5.2]
     end 
   end
 end
+
+class ShoeColor 
+  belong_to :color
+  belong_to :shoe
+end 
+
+class Color 
+  has_many :shoes, through: :shoecolor
+end 
+
+class Shoe
+  belong_to :owner
+  #they have foreign key 
+end 
+
+class Owner
+  has_many :shoes
+end 
